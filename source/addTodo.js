26f56508f9662a965cb5adb4a6c40de362062c42
nansmatty <narayanmaitysp1997@gmail.com>
2022-01-26 18:21:45 +0530

    first lambda configured todoApp

--- source/addTodo.js
+++ source/addTodo.js
@@ -1,19 +1,34 @@
-'use strict';
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+const { DynamoDB } = require('aws-sdk');
 
-const hello = async (event) => {
+const addTodo = async (event) => {
+	const dynamoDB = AWS.DynamoDB.DocumentClient();
+
+	const { todo } = JSON.parse(event.body);
+	const createdAt = new Date();
+	const id = uuidv4();
+
+	console.log('This is an ID', id);
+
+	const newTodo = {
+		id,
+		todo,
+		createdAt,
+		completed: false,
+	};
+
+	await dynamoDB.put({
+		TableName: 'TodoTable',
+		Item: newTodo,
+	});
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: 'Go Serverless v2.0! Your function executed successfully!',
-				input: event,
-			},
-			null,
-			2
-		),
+		body: JSON.stringify(newTodo),
 	};
 };
 
 module.exports = {
-	handler: hello,
+	handler: addTodo,
 };
